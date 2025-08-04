553020d8035f75d1a39cabedf9cfd114af1175c0
HazemAlMelli <hazemalmile7@gmail.com>
2025-08-04 22:16:22 +0300

    update Header & Footer

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,40 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import style from '../style/Navbar.module.css'; // Assuming you have a CSS module for styling
-
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import style from "../style/Navbar.module.css"; // Assuming you have a CSS module for styling
 
 const Navbar = () => {
   return (
-   <>
-    <header className={style.header}>
-     
-   
-        
-    
+    <>
+      <header className={style.header}>
         <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/Home"><p className={style.logo}>3KAR</p></Link></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><Link to="/products">Products</Link></li>
-            <li><Link to="/cart">Cart</Link></li>
-        </ul>
-     
-   </header>
-   </>
-  );
-}
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/team">Our Team</Link>
+          </li>
+          <li>
+            <Link to="/villas">Villas</Link>
+          </li>
+          <li>
+            <Link to="/Home">
+              <p className={style.logo}>Aqar</p>
+            </Link>
+          </li>
 
-export default Navbar
+          <li>
+            <Link to="/apartments">Apartments</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+          <li>
+            <Link to="/login">login</Link>
+          </li>
+        </ul>
+      </header>
+    </>
+  );
+};
+
+export default Navbar;
